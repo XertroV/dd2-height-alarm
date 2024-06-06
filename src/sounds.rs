86e2ba18c1ad7b12fda9ab709f481568d6ddebf1
Max Kaye <m@xk.io>
2024-06-06 16:56:39 +1000

    add more sounds

--- src/sounds.rs
+++ src/sounds.rs
@@ -85,3 +85,135 @@ pub fn get_guy_thirteen() -> &'static [u8] {
 pub fn get_guy_someone() -> &'static [u8] {
     include_bytes!("../assets/sounds/someone.ogg")
 }
+
+// group 2 files:
+// bijanz.ogg  carljr.ogg  dam.ogg    five.ogg  granadyy.ogg         hefest.ogg  ixxonn.ogg  jxliano.ogg  massa.ogg  nine.ogg  samifying.ogg  six.ogg       talliebird.ogg  tres.ogg  tylerz.ogg  warl.ogg
+// birdie.ogg  clover.ogg  eight.ogg  four.ogg  gunafunkurmonky.ogg  hugo.ogg    jave.ogg    marius.ogg   mtat.ogg   one.ogg   seven.ogg      skandear.ogg  three.ogg       two.ogg   wally.ogg   youronlyhope7.ogg
+
+pub fn get_guy_bijanz() -> &'static [u8] {
+    include_bytes!("../assets/sounds/bijanz.ogg")
+}
+
+pub fn get_guy_carljr() -> &'static [u8] {
+    include_bytes!("../assets/sounds/carljr.ogg")
+}
+
+pub fn get_guy_dam() -> &'static [u8] {
+    include_bytes!("../assets/sounds/dam.ogg")
+}
+
+pub fn get_guy_five() -> &'static [u8] {
+    include_bytes!("../assets/sounds/five.ogg")
+}
+
+pub fn get_guy_granadyy() -> &'static [u8] {
+    include_bytes!("../assets/sounds/granadyy.ogg")
+}
+
+pub fn get_guy_hefest() -> &'static [u8] {
+    include_bytes!("../assets/sounds/hefest.ogg")
+}
+
+pub fn get_guy_ixxonn() -> &'static [u8] {
+    include_bytes!("../assets/sounds/ixxonn.ogg")
+}
+
+pub fn get_guy_jxliano() -> &'static [u8] {
+    include_bytes!("../assets/sounds/jxliano.ogg")
+}
+
+pub fn get_guy_massa() -> &'static [u8] {
+    include_bytes!("../assets/sounds/massa.ogg")
+}
+
+pub fn get_guy_nine() -> &'static [u8] {
+    include_bytes!("../assets/sounds/nine.ogg")
+}
+
+pub fn get_guy_samifying() -> &'static [u8] {
+    include_bytes!("../assets/sounds/samifying.ogg")
+}
+
+pub fn get_guy_six() -> &'static [u8] {
+    include_bytes!("../assets/sounds/six.ogg")
+}
+
+pub fn get_guy_talliebird() -> &'static [u8] {
+    include_bytes!("../assets/sounds/talliebird.ogg")
+}
+
+pub fn get_guy_tres() -> &'static [u8] {
+    include_bytes!("../assets/sounds/tres.ogg")
+}
+
+pub fn get_guy_tylerz() -> &'static [u8] {
+    include_bytes!("../assets/sounds/tylerz.ogg")
+}
+
+pub fn get_guy_warl() -> &'static [u8] {
+    include_bytes!("../assets/sounds/warl.ogg")
+}
+
+pub fn get_guy_birdie() -> &'static [u8] {
+    include_bytes!("../assets/sounds/birdie.ogg")
+}
+
+pub fn get_guy_clover() -> &'static [u8] {
+    include_bytes!("../assets/sounds/clover.ogg")
+}
+
+pub fn get_guy_eight() -> &'static [u8] {
+    include_bytes!("../assets/sounds/eight.ogg")
+}
+
+pub fn get_guy_four() -> &'static [u8] {
+    include_bytes!("../assets/sounds/four.ogg")
+}
+
+pub fn get_guy_gunafunkurmonky() -> &'static [u8] {
+    include_bytes!("../assets/sounds/gunafunkurmonky.ogg")
+}
+
+pub fn get_guy_hugo() -> &'static [u8] {
+    include_bytes!("../assets/sounds/hugo.ogg")
+}
+
+pub fn get_guy_jave() -> &'static [u8] {
+    include_bytes!("../assets/sounds/jave.ogg")
+}
+
+pub fn get_guy_marius() -> &'static [u8] {
+    include_bytes!("../assets/sounds/marius.ogg")
+}
+
+pub fn get_guy_mtat() -> &'static [u8] {
+    include_bytes!("../assets/sounds/mtat.ogg")
+}
+
+pub fn get_guy_one() -> &'static [u8] {
+    include_bytes!("../assets/sounds/one.ogg")
+}
+
+pub fn get_guy_seven() -> &'static [u8] {
+    include_bytes!("../assets/sounds/seven.ogg")
+}
+
+pub fn get_guy_skandear() -> &'static [u8] {
+    include_bytes!("../assets/sounds/skandear.ogg")
+}
+
+pub fn get_guy_three() -> &'static [u8] {
+    include_bytes!("../assets/sounds/three.ogg")
+}
+
+pub fn get_guy_two() -> &'static [u8] {
+    include_bytes!("../assets/sounds/two.ogg")
+}
+
+pub fn get_guy_wally() -> &'static [u8] {
+    include_bytes!("../assets/sounds/wally.ogg")
+}
+
+pub fn get_guy_youronlyhope7() -> &'static [u8] {
+    include_bytes!("../assets/sounds/youronlyhope7.ogg")
+}
